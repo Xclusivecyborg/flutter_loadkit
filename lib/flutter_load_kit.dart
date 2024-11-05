7378e0851f45a4edfe193020a1aa5ab9f1cbf01e
Xclusivecyborg <xclusivecyborg@gmail.com>
2024-11-05 14:58:46 +0100

    FEAT: added loadkit aligning square

--- lib/flutter_load_kit.dart
+++ lib/flutter_load_kit.dart
@@ -1,12 +1,12 @@
 library flutter_load_kit;
 
-export 'src/loadkit_spinning_arcs.dart';
 export 'src/loadkit_360.dart';
+export 'src/loadkit_aligning_square.dart';
+export 'src/loadkit_filled_circle.dart';
+export 'src/loadkit_folding_squares.dart';
+export 'src/loadkit_line_chase.dart';
 export 'src/loadkit_pulse_lines.dart';
 export 'src/loadkit_rotating_lines.dart';
 export 'src/loadkit_rotation_arcs.dart';
 export 'src/loadkit_scaling_wave.dart';
-export 'src/loadkit_folding_squares.dart';
-export 'src/loadkit_filled_circle.dart';
-export 'src/loadkit_water_droplet.dart';
-export 'src/loadkit_line_chase.dart';
+export 'src/loadkit_spinning_arcs.dart';
---
+++ lib/src/loadkit_aligning_square.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class LoadkitAligningSquare extends StatefulWidget {
+  const LoadkitAligningSquare({
+    super.key,
+    this.size = 50.0,
+    this.color = Colors.white,
+    this.borderColor,
+    this.backgroundColor = Colors.black,
+    this.duration = const Duration(milliseconds: 300),
+  });
+  final double size;
+  final Color? color, borderColor, backgroundColor;
+  final Duration duration;
+
+  @override
+  State<LoadkitAligningSquare> createState() => _LoadkitAligningSquareState();
+}
+
+class _LoadkitAligningSquareState extends State<LoadkitAligningSquare> {
+  Alignment _alignment = Alignment.topLeft;
+  Timer? _timer;
+
+  void getNextAlignMent() {
+    final List<Alignment> alignments = [
+      Alignment.topLeft,
+      Alignment.topRight,
+      Alignment.bottomRight,
+      Alignment.bottomLeft,
+    ];
+    final nextIndex = (alignments.indexOf(_alignment) + 1) % alignments.length;
+
+    setState(() {
+      if (nextIndex == alignments.length) {
+        _alignment = alignments[0];
+      } else {
+        _alignment = alignments[nextIndex];
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(widget.duration, (timer) {
+      getNextAlignMent();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: widget.backgroundColor ?? Colors.red,
+        ),
+        height: widget.size,
+        width: widget.size,
+        child: AnimatedAlign(
+          duration: Duration(milliseconds: widget.duration.inMilliseconds ~/ 5),
+          alignment: _alignment,
+          child: Container(
+            height: widget.size * 0.5,
+            width: widget.size * 0.5,
+            color: widget.color ?? Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
